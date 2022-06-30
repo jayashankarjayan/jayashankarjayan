157c47304c3ba99b463be1220fec1b6073d18ad3
jayashankar.jayan <jayashankar.jayan@gmail.com>
2022-06-30 20:22:50 +0530

    separated page for multiple projects and single projects

--- application/projects/routes.py
+++ application/projects/routes.py
@@ -3,5 +3,9 @@ from flask import Blueprint, render_template
 BLUEPRINT_PROJECTS = Blueprint("projects", __name__)
 
 @BLUEPRINT_PROJECTS.route("/", methods=["GET"])
-def render_resume():
+def get_all_projects():
+    return render_template("multiple-projects.html")
+
+@BLUEPRINT_PROJECTS.route("/<int:project_id>", methods=["GET"])
+def get_single_project(project_id):
     return render_template("single-project.html")
