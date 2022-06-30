e511d613a0898f1d64173ca5378a4597147db906
jayashankar.jayan <jayashankar.jayan@gmail.com>
2022-06-30 23:47:30 +0530

    correcting db url

--- application/utils/constants.py
+++ application/utils/constants.py
@@ -7,5 +7,6 @@ DB_PASS = os.environ.get("POSTGRES_PASS")
 DB_NAME =  os.environ.get("PORTFOLIO_DB_NAME")
 
 DB_CONNECTION_STRING = os.getenv("DATABASE_URL", f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+DB_CONNECTION_STRING = DB_CONNECTION_STRING.replace("postgres", "postgresql")
 
 BASE_SWAGGER_FOLDER = os.path.join(os.getcwd(), "swagger")
