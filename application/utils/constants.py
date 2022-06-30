9df3492ecf462b4d92017aa2a82c8374d5563422
jayashankar.jayan <jayashankar.jayan@gmail.com>
2022-06-30 23:44:58 +0530

    using env var for db url

--- application/utils/constants.py
+++ application/utils/constants.py
@@ -5,6 +5,7 @@ DB_PORT = os.environ.get("POSTGRES_PORT")
 DB_USER = os.environ.get("POSTGRES_USER")
 DB_PASS = os.environ.get("POSTGRES_PASS")
 DB_NAME =  os.environ.get("PORTFOLIO_DB_NAME")
-DB_CONNECTION_STRING = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+DB_CONNECTION_STRING = os.getenv("DATABASE_URL", f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
 BASE_SWAGGER_FOLDER = os.path.join(os.getcwd(), "swagger")
