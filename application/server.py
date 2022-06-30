416c0d6fac7dd2d5b3336836d16d58c896c79daa
jayashankar.jayan <jayashankar.jayan@gmail.com>
2022-06-30 15:49:29 +0530

    added projects routes and base page

--- application/server.py
+++ application/server.py
@@ -5,12 +5,14 @@ from flasgger import Swagger
 
 from home.routes import BLUEPRINT_HOME
 from resume.routes import BLUEPRINT_RESUME
+from projects.routes import BLUEPRINT_PROJECTS
 
 APP = Flask(__name__, static_folder='static', static_url_path='')
 swagger = Swagger(APP)
 
 APP.register_blueprint(BLUEPRINT_HOME, url_prefix="/home")
 APP.register_blueprint(BLUEPRINT_RESUME, url_prefix="/resume")
+APP.register_blueprint(BLUEPRINT_PROJECTS, url_prefix="/projects")
 
 if __name__ == "__main__":
     port = os.getenv("PORT", 5000)
